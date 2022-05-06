cdfb99ce482ad963c74e195b3aab15bafae175ca
James Kirwa <james.kirwa@twigafoods.com>
2022-05-06 18:05:11 +0300

    - Koin setup

---
+++ app/src/main/java/com/jkirwa/penguinpay/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.jkirwa.penguinpay.di
+
+import com.jkirwa.penguinpay.feature_send_money.data.data_source.remote.api.ExchangeRateApiService
+import com.jkirwa.penguinpay.feature_send_money.data.repository.ExchangeRatesRepository
+import com.jkirwa.penguinpay.feature_send_money.domain.repository.ExchangeRatesRepositoryImpl
+import com.jkirwa.penguinpay.feature_send_money.domain.utils.Constants
+import com.jkirwa.penguinpay.feature_send_money.presentation.viewmodel.ExchangeRatesViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+
+const val baseUrl: String = Constants.BASE_URL
+const val apiKey: String = Constants.APIKEY
+
+val appModule = module {
+    single { createNetworkClient(baseUrl, apiKey) }
+    single { (get() as? Retrofit)?.create(ExchangeRateApiService::class.java) }
+
+    factory<ExchangeRatesRepository> {
+        ExchangeRatesRepositoryImpl(
+            exchangeRateApiService = get()
+        )
+    }
+
+    viewModel {
+        ExchangeRatesViewModel(exchangeRatesRepository = get())
+    }
+}
